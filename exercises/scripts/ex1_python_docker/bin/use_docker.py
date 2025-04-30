ab49509d6adefbc2dafd32969fc352b6d3ea6b0e
grinic <nicola.gritti.86@gmail.com>
2025-04-30 16:36:57 +0200

    Update use_docker.py

--- exercises/scripts/ex1_python_docker/bin/use_docker.py
+++ exercises/scripts/ex1_python_docker/bin/use_docker.py
@@ -4,4 +4,4 @@ if __name__=="__main__":
   import numpy as np
   
   img = np.random.randn(100,100)
-  print(f"Mean of the random image: {np.mean(img)}")
+  print(f"Mean of the random image, {np.mean(img)}")
